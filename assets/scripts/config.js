e876e19a94a72b35abd22a316fdff184125db16b
xiangcatherine <xiang.catherine@gmail.com>
2017-04-11 10:15:44 -0400

    Changes production URL in config.js
    Took off '/' at the end of the herokuapp URL, because while auth was
    working in local server, it was sending an error when I went to the
    production site. There were two backslashes, and deleting the
    backslash solved the problem.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://aqueous-atoll-85096.herokuapp.com/',
+    production: 'https://aqueous-atoll-85096.herokuapp.com',
     development: 'http://localhost:4741/'
   }
 }
