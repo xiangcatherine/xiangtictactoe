27da468157602ba41c32731287038ceefa143677
xiangcatherine <xiang.catherine@gmail.com>
2017-04-11 11:29:47 -0400

    Removed unnecessary "clicks.js" previously created for click events

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,7 +13,6 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 require('./example')
 require('./gameboard/main.js')
-require('./gameboard/clicks.js')
 
 const authEvents = require('./auth/events.js')
 
